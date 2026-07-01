6994a5ee955308d7a856c65425cb33bcc3bc3b8d
Luiz Liesch <luiz271269@gmail.com>
2026-07-01 13:37:46 +0000

    Visual edits

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -133,7 +133,7 @@ export default function Layout() {
           <PainelNotificacoes />
         </div>
         <Breadcrumb location={location} />
-        <main className="flex-1 overflow-auto">
+        <main className="flex-1 overflow-auto hidden">
           <Outlet />
         </main>
         {/* Footer */}
