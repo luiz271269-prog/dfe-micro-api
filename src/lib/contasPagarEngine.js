d7d1f072794cc0834db6bd0d2e143464bc6d49ce
Luiz Liesch <luiz271269@gmail.com>
2026-09-09 17:49:28 +0000

    Adicionar opção de recorrência semanal

--- src/lib/contasPagarEngine.js
+++ src/lib/contasPagarEngine.js
@@ -61,7 +61,6 @@ export function consolidarContasPagar({ despesas = [], tributos = [], folhas = [
       data_vencimento: d.data_vencimento || d.data,
       empresa: d.empresa,
       forma_pagamento: d.forma_pagamento,
-      recorrente: !!d.recorrente,
       ...eixos(d, 'despesa'),
     });
   });
--- src/lib/contasPagasEngine.js
+++ src/lib/contasPagasEngine.js
@@ -24,7 +24,6 @@ export function consolidarContasPagas({ despesas = [], tributos = [], folhas = [
       data_emissao: d.data,
       data_pagamento: d.data,
       empresa: d.empresa, forma_pagamento: d.forma_pagamento,
-      recorrente: !!d.recorrente,
       ...eixos(d, 'despesa'),
     });
   });
